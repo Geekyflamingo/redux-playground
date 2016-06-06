56e0e25bbefa484526fa4617b39a0bbdf4f79701
Becca <rebecca.vasil@gmail.com>
2016-06-06 16:20:55 -0600

    added support for change search text action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,10 +3,24 @@ var redux = require('redux');
 console.log('Starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
-	// state = state || {name: 'Anonymous'};
-	return state;
+	switch (action.type) {
+		case 'CHANGE_NAME':
+			return {
+				...state,
+				name: action.name
+			};
+		default:
+			return state;
+	}
 };
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('current state', currentState);
+
+store.dispatch({
+	type: 'CHANGE_NAME',
+	name: 'Becca'
+});
+
+console.log('name should be becca', store.getState());
